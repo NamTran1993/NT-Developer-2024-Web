7a46645d1d727bf19db94b55db74792b08c21c38
Nam Tran <tranhuynam93@gmail.com>
2024-03-13 23:05:09 +0700

    code send mail (#1)
    Co-authored-by: nam <nam@DESKTOP-6AOJ5BJ>

---
+++ UluruVietNam/App_Code/Model/MailModel.cs
@@ -0,0 +1,172 @@
+﻿
+public class MailModel
+{
+    private string _smtpServerName = string.Empty;
+    private int _port = 0;
+    private string _smtpUserName = string.Empty;
+    private string _smtpPassword = string.Empty;
+    private bool _smtpAuth = false;
+    private string _fromMailAddress = string.Empty;
+    private string _sendMailAddress = string.Empty;
+    private string _bcc = string.Empty;
+    private string _subject = string.Empty;
+    private string _body = string.Empty;
+    private string _attach = string.Empty;
+    private bool _secureConnect = false;
+
+    public string SmtpServerName
+    {
+        get
+        {
+            return _smtpServerName;
+        }
+
+        set
+        {
+            _smtpServerName = value;
+        }
+    }
+
+    public int Port
+    {
+        get
+        {
+            return _port;
+        }
+
+        set
+        {
+            _port = value;
+        }
+    }
+
+    public string SmtpUserName
+    {
+        get
+        {
+            return _smtpUserName;
+        }
+
+        set
+        {
+            _smtpUserName = value;
+        }
+    }
+
+    public string SmtpPassword
+    {
+        get
+        {
+            return _smtpPassword;
+        }
+
+        set
+        {
+            _smtpPassword = value;
+        }
+    }
+
+    public bool SmtpAuth
+    {
+        get
+        {
+            return _smtpAuth;
+        }
+
+        set
+        {
+            _smtpAuth = value;
+        }
+    }
+
+    public string FromMailAddress
+    {
+        get
+        {
+            return _fromMailAddress;
+        }
+
+        set
+        {
+            _fromMailAddress = value;
+        }
+    }
+
+    public string SendMailAddress
+    {
+        get
+        {
+            return _sendMailAddress;
+        }
+
+        set
+        {
+            _sendMailAddress = value;
+        }
+    }
+
+    public string Bcc
+    {
+        get
+        {
+            return _bcc;
+        }
+
+        set
+        {
+            _bcc = value;
+        }
+    }
+
+    public string Subject
+    {
+        get
+        {
+            return _subject;
+        }
+
+        set
+        {
+            _subject = value;
+        }
+    }
+
+    public string Body
+    {
+        get
+        {
+            return _body;
+        }
+
+        set
+        {
+            _body = value;
+        }
+    }
+
+    public string Attach
+    {
+        get
+        {
+            return _attach;
+        }
+
+        set
+        {
+            _attach = value;
+        }
+    }
+
+    public bool SecureConnect
+    {
+        get
+        {
+            return _secureConnect;
+        }
+
+        set
+        {
+            _secureConnect = value;
+        }
+    }
+}
---
+++ UluruVietNam/App_Code/Utility/MailUtility.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Net;
+using System.Net.Mail;
+using System.Net.Mime;
+using System.Text;
+
+public class MailUtility
+{
+    private MailModel _mailModel = null;
+
+    public MailUtility(MailModel mailModel)
+    {
+        this._mailModel = mailModel;
+    }
+
+    public bool SendMail()
+    {
+        try
+        {
+            ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls | (SecurityProtocolType)768 | (SecurityProtocolType)3072;
+            SmtpClient smtp = new SmtpClient();
+            MailMessage msg = new MailMessage();
+            Encoding myEnc = Encoding.GetEncoding("iso-2022-jp");
+
+            smtp.Host = this._mailModel.SmtpServerName;
+            smtp.Port = this._mailModel.Port;
+            smtp.UseDefaultCredentials = false;
+
+            if (this._mailModel.SmtpAuth)
+            {
+                smtp.Credentials = new NetworkCredential(this._mailModel.SmtpUserName, this._mailModel.SmtpPassword);
+            }
+
+            smtp.ServicePoint.MaxIdleTime = 1000;
+
+            if (this._mailModel.SecureConnect)
+            {
+                smtp.EnableSsl = true;
+            }
+
+            msg.From = new MailAddress(this._mailModel.FromMailAddress);
+            msg.To.Add(new MailAddress(this._mailModel.SendMailAddress));
+            msg.Subject = this._mailModel.Subject; 
+
+            if (this._mailModel.Bcc != "")
+            {
+                msg.Bcc.Add(new MailAddress(this._mailModel.Bcc));
+            }
+
+            string sBody = this._mailModel.Body;
+            AlternateView altView = AlternateView.CreateAlternateViewFromString(sBody, myEnc, MediaTypeNames.Text.Plain);
+            altView.TransferEncoding = TransferEncoding.SevenBit;
+
+            msg.AlternateViews.Add(altView);
+
+            if (this._mailModel.Attach != string.Empty)
+            {
+                Attachment attach1 = new Attachment(this._mailModel.Attach);
+                msg.Attachments.Add(attach1);
+            }
+
+            smtp.Send(msg);
+            return true;
+        }
+        catch (Exception ex)
+        { }
+        return false;
+    }
+}
